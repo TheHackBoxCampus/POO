adf1b3e8d889694d721c02960265323d203dc95e
TheHackBoxCampus <126725038+TheHackBoxCampus@users.noreply.github.com>
2023-04-13 09:33:13 -0500

    feat: :sparkles: Cards
    I added logic so that when submit is done the card is added
    
    class, styles, methods

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+let template = color => dimension => brand => eraser => material => {
+    return (`
+        <tr>
+            <td><input type="color" value=${color}></td>
+            <td>${dimension}</td>
+            <td>${brand}</td>
+            <td>${eraser}</td>
+            <td>${material}</td>
+        </tr>
+    `)
+}
+
 class lapiz{
     #marca
     constructor(
@@ -9,20 +21,45 @@ class lapiz{
         marca="mongol"
     })
     {
-        this.attr = [color, dimension, borrador, material, marca]
+        this.attr = [color, dimension, marca, borrador, material]
+        this.container = document.querySelector('#tbody')
     }
+
+    showDataForDefault(){
+        let colorHTLM = document.querySelector('[name=""]')
+        let dimensionHTML = document.querySelector('[name="dimension"]')
+        let marcaHTML = document.querySelectorAll('[name="marca"]')
+        let borradorHTML = document.querySelectorAll('[name="borrador"]')
+        let materialHTML = document.querySelectorAll('[name="material"]')
+        console.log(marcaHTML)
+    }
+
+    updateDatasForDefault(){
+       color = this.attr[0]
+       dimension = this.attr[1]
+       borrador = this.attr[2]
+       material = this.attr[3]
+       marca = this.attr[4]
+    }    
+
     getElementsDom(){
         let data = {}
         document.querySelector('form').addEventListener('submit', e => {
             e.preventDefault()
             data = Object.fromEntries(new FormData(e.target))
-            return data
-       })        
+            this.AddElementTable(data)
+        })
     }
-    AddElementTable() {
-        let dataForm = this.getElementsDom()
-        console.log(dataForm)
+    AddElementTable(obj) {
+        this.attr = []
+        for(let prop in obj) this.attr.push(obj[prop])
+        let htmlRender = template(this.attr[0])(this.attr[1])(this.attr[2])(this.attr[3])(this.attr[4])
+        this.container.insertAdjacentHTML('beforeend', htmlRender)
     }
 }
-let obj = new lapiz({});
 
+addEventListener('DOMContentLoaded', () => {
+    let obj = new lapiz({})
+    obj.showDataForDefault()
+    obj.getElementsDom()
+})
